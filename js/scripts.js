c2dd62a072ee532496eab8195d90fe246fc264c9
timkellogg <tim.kellogg@gmail.com>
2015-09-15 10:17:35 -0700

    add method to withdraw money

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function Account(name, balance) {
   this.name = name,
   this.balance = balance
 };
+
+Account.prototype.withdraw = function(amount) {
+  return this.balance -= amount; 
+};
--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,11 @@ describe("Account", function() {
     expect(testAccount.name).to.equal("Name");
     expect(testAccount.balance).to.equal(100);
   });
+
+  it("adds the withdraw method to all accounts", function() {
+    var testAccount = new Account("Nomen", 200);
+    testAccount.withdraw(50);
+    expect(testAccount.balance).to.equal(150);
+  });
 });
+
